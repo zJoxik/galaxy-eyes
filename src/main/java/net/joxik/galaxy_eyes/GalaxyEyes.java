5ea058b143f2ef8455dd81b0bc389239f7c42632
Joxik <97808529+zJoxik@users.noreply.github.com>
2022-06-08 20:01:52 +0200

    Initial commit

---
+++ src/main/java/net/joxik/galaxy_eyes/GalaxyEyes.java
@@ -0,0 +1,26 @@
+package net.joxik.galaxy_eyes;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.option.KeyBinding;
+
+import org.lwjgl.glfw.GLFW;
+
+public class GalaxyEyes implements ClientModInitializer {
+
+	private static KeyBinding keyBind;
+
+	public static final double zoomLevel = 0.23;
+
+	@Override
+	public void onInitializeClient() {
+		keyBind = new KeyBinding("Toggle Zoom", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_C, "Galaxy Eyes");
+
+		KeyBindingHelper.registerKeyBinding(keyBind);
+	}
+
+	public static boolean isZooming() {
+		return keyBind.isPressed();
+	}
+}
---
+++ src/main/java/net/joxik/galaxy_eyes/mixin/GalaxyEyesMixin.java
@@ -0,0 +1,26 @@
+package net.joxik.galaxy_eyes.mixin;
+
+import net.joxik.galaxy_eyes.GalaxyEyes;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+import net.minecraft.client.render.GameRenderer;
+
+
+@Environment(EnvType.CLIENT)
+@Mixin(GameRenderer.class)
+public class GalaxyEyesMixin {
+	@Inject(method = "getFov(Lnet/minecraft/client/render/Camera;FZ)D", at = @At("RETURN"), cancellable = true)
+	public void getZoomLevel(CallbackInfoReturnable<Double> info) {
+		if (GalaxyEyes.isZooming()) {
+			double fov = info.getReturnValue();
+			info.setReturnValue(fov * GalaxyEyes.zoomLevel);
+		}
+	}
+}
