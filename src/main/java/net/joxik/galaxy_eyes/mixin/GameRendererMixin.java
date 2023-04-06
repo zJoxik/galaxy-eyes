389978c5b668e25058c84bfec90a14e085eade93
Joxik <97808529+zJoxik@users.noreply.github.com>
2023-04-06 21:42:50 +0200

    Refactor codebase and rename GalaxyEyesMixin
    Rename GalaxyEyesMixin to GameRendererMixin

--- src/main/java/net/joxik/galaxy_eyes/mixin/GameRendererMixin.java
+++ src/main/java/net/joxik/galaxy_eyes/mixin/GameRendererMixin.java
@@ -2,25 +2,23 @@ package net.joxik.galaxy_eyes.mixin;
 
 import net.joxik.galaxy_eyes.GalaxyEyes;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 
 import net.minecraft.client.render.GameRenderer;
 
 
 @Environment(EnvType.CLIENT)
 @Mixin(GameRenderer.class)
-public class GalaxyEyesMixin {
+public class GameRendererMixin {
     @Inject(method = "getFov(Lnet/minecraft/client/render/Camera;FZ)D", at = @At("RETURN"), cancellable = true)
-    public void getZoomLevel(CallbackInfoReturnable<Double> info) {
+    public void getFov(CallbackInfoReturnable<Double> cir) {
         if (GalaxyEyes.isZooming()) {
-            double fov = info.getReturnValue();
-            info.setReturnValue(fov * GalaxyEyes.zoomLevel);
+            cir.setReturnValue(GalaxyEyes.getZoomFov());
         }
     }
 }
